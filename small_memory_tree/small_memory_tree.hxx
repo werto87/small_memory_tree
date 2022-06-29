080f061ae63f6e9449e229eff7165ccc7a1e14b7
werto87 <huhulasmichinruhe@web.de>
2022-06-30 00:17:58 +0900

    convinience header

---
+++ small_memory_tree/small_memory_tree.hxx
@@ -0,0 +1,6 @@
+#ifndef B45725F4_6A46_49F5_A1F5_274D0EFBC314
+#define B45725F4_6A46_49F5_A1F5_274D0EFBC314
+
+#include "small_memory_tree/treeToVector.hxx"
+
+#endif /* B45725F4_6A46_49F5_A1F5_274D0EFBC314 */
