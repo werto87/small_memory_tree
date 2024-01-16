25aff879975c8e2e47d41cab0140f8a2d760d1c0
werto87 <huhulasmichinruhe@web.de>
2024-01-16 19:23:40 +0900

    conan 2 support

---
+++ conanfile.py
@@ -0,0 +1,18 @@
+from conan import ConanFile
+
+
+class Project(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def configure(self):
+        # We can control the options of our dependencies based on current options
+        self.options["catch2"].with_main = True
+        self.options["catch2"].with_benchmark = True
+        self.options["boost"].header_only = True
+
+    def requirements(self):
+        self.requires("boost/1.83.0", force=True)
+        self.requires("st_tree/1.2.1")
+        self.requires("range-v3/0.12.0")
+        self.requires("catch2/2.13.7")
