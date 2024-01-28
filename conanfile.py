4bec1501075723fe3c12db7cf4f25adc685ae534
werto87 <huhulasmichinruhe@web.de>
2024-01-28 11:16:28 +0900

    cmake update

--- conanfile.py
+++ conanfile.py
@@ -6,13 +6,11 @@ class Project(ConanFile):
     generators = "CMakeToolchain", "CMakeDeps"
 
     def configure(self):
-        # We can control the options of our dependencies based on current options
         self.options["catch2"].with_main = True
         self.options["catch2"].with_benchmark = True
-        self.options["boost"].header_only = True
 
     def requirements(self):
-        self.requires("boost/1.83.0", force=True)
+        self.requires("boost/1.84.0", force=True)
         self.requires("st_tree/1.2.1")
         self.requires("catch2/2.13.7")
         self.requires("confu_algorithm/0.0.1")
