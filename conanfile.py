dba01ae279daa9863a28d5021e4bc1987d98f917
walde <w-schneider1987@web.de>
2024-02-25 10:55:36 +0900

    provide compile_commands.json. added presets

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,17 @@
 from conan import ConanFile
-
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+from conan.tools.files import collect_libs, rmdir
 
 class Project(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators =  "CMakeDeps"
+
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False #workaround because this leads to useless options in cmake-tools configure
+        tc.generate()
 
     def configure(self):
         self.options["catch2"].with_main = True
