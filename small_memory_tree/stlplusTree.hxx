454068a07dbf5bf14e016c294b7000bd7c871342
werto87 <huhulasmichinruhe@web.de>
2024-05-13 19:34:44 +0200

    base class adapter

--- small_memory_tree/stlplusTree.hxx
+++ small_memory_tree/stlplusTree.hxx
@@ -5,7 +5,7 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
 
-#include "SmallMemoryTreeAdapter.hxx"
+#include "smallMemoryTreeAdapter.hxx"
 #include <stlplus/containers/ntree.hpp>
 #include <stlplus/strings/print_basic.hpp>
 #include <stlplus/strings/print_int.hpp>
