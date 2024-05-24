f07e98b73e0a59af33893b4ce6f3cee49a130a94
werto87 <huhulasmichinruhe@web.de>
2024-05-24 10:52:12 +0200

    removed not needed includes

--- small_memory_tree/stlplusTree.hxx
+++ small_memory_tree/stlplusTree.hxx
@@ -10,10 +10,6 @@ Distributed under the Boost Software License, Version 1.0.
 #include <cstdint>
 #include <deque>
 #include <stlplus/containers/ntree.hpp>
-#include <stlplus/strings/print_basic.hpp>
-#include <stlplus/strings/print_int.hpp>
-#include <stlplus/strings/print_ntree.hpp>
-#include <stlplus/strings/print_string.hpp>
 #include <type_traits>
 namespace small_memory_tree
 {
