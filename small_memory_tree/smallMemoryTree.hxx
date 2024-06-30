9d9d9b1d7cb5aab7eafa25f3fd5440b6f53f6c93
werto87 <huhulasmichinruhe@web.de>
2024-06-30 13:31:05 +0200

    License

--- small_memory_tree/smallMemoryTree.hxx
+++ small_memory_tree/smallMemoryTree.hxx
@@ -4,6 +4,7 @@ Copyright (c) 2024 Waldemar Schneider (w-schneider1987 at web dot de)
 Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE.md or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
+
 #include <algorithm>
 #include <boost/numeric/conversion/cast.hpp>
 #include <confu_algorithm/binaryFind.hxx>
