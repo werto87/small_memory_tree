377ef0c08ff5053130efe0d8ba173e2f9852600b
werto87 <huhulasmichinruhe@web.de>
2024-12-29 23:31:15 +0100

    dep version bump

--- conanfile.py
+++ conanfile.py
@@ -19,6 +19,6 @@ class Project(ConanFile):
         self.requires("boost/1.85.0", force=True)
         self.requires("st_tree/1.2.2")
         self.requires("catch2/2.13.7")
-        self.requires("confu_algorithm/1.1.1")
+        self.requires("confu_algorithm/[<2]")
         self.requires("stlplus/3.16.0")
         
